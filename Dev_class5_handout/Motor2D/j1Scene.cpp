d367e34571b94032675ef3c01fe2b6e3cf42a3ea
Skyway666 <lucasgarciamateu@gmail.com>
2017-12-26 17:18:02 +0100

    Trying to fix gitignore

--- Dev_class5_handout/Motor2D/j1Scene.cpp
+++ Dev_class5_handout/Motor2D/j1Scene.cpp
@@ -62,7 +62,7 @@ bool j1Scene::Update(float dt)
 		App->render->camera.x -= 1;
 
 	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		App->render->camera.x += 1;
+		App->render->camera.x += 2;
 	
 	if (App->render->camera.y < App->map->data.camera_y_limit)
 	{
